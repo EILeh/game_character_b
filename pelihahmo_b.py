7f4329a01ea77f8d930402bd38593bdc3afbc9ec
Elli Lehtimäki <elli.i.lehtimaki@gmail.com>
2023-05-10 11:25:40 +0300

    Commented code

--- pelihahmo_b.py
+++ pelihahmo_b.py
@@ -1,7 +1,15 @@
 """
-COMP.CS.100 Pelihahmo B
-Tekijä: Elli Lehtimäki
-Opiskelijanumero: 151309919
+Game character B
+
+Often in video games there are multiple characters who have some kind of
+interaction with each other. Often this is violent, but not always. The
+caracters in a game can, for example, pass items between each other or have a
+fight. In the program the character has hitpoints and items and the weapons
+have an amount that causes damage to other character's hitpoints. If the
+hitpoints go under zero, the character is destroyed.
+
+Writer of the program: EILeh
+
 """
 
 class Character:
@@ -10,19 +18,13 @@ class Character:
     he or she can do.
     """
 
-    # TODO: copy your Character class implementation from
-    #       the previous assignment here and implement the
-    #       following new methods.
-    #
-    #       Also note, that you have to modify at least
-    #       __init__ and printout methods to conform with
-    #       the new behavior of the class.
-
     def __init__(self, character_name, hitpoints,
                  starting_items = None):
 
         self.__name = character_name
 
+        # Initializes an empty dictionary if the character doesn't have any
+        # items.
         if starting_items is None:
             starting_items = {}
 
@@ -30,50 +32,49 @@ class Character:
 
         self.__hitpoints = hitpoints
 
-        # self.__item_dict = item_dict
-
-        # self.__give_item = {}
-
-
     def printout(self):
 
+        # If character doesn't have items, only character's name is printed.
         if not self.__item_dict:
             print("Name:", self.__name)
             print("  --nothing--")
 
         else:
-
             print("Name:", self.__name)
             print("Hitpoints:", self.__hitpoints)
 
+            # Goes through the character's item that are in a dictionary and
+            # prints the item and the amount of items.
             for item in sorted(self.__item_dict):
                 print(" ", self.__item_dict[item], item)
-
-
 
     def get_name(self):
 
         return self.__name
 
-
     def give_item(self, test_item):
 
+        # If character already has the given item, the item's value is grown
+        # by one.
         if test_item in self.__item_dict:
             self.__item_dict[test_item] += 1
 
+        # If character doesn't have the given item, a new key-value pair it
+        # added to the dictionary __items.
         else:
             self.__item_dict[test_item] = 1
 
-
-
     def remove_item(self, item_to_be_removed):
 
+        # If character already has the given item, the item's value is reduced
+        # by one.
         if item_to_be_removed in self.__item_dict:
             self.__item_dict[item_to_be_removed] -= 1
 
+            # If character had only one of the items that was reduced,
+            # the item itself is removed from the dictionary.
             if self.__item_dict[item_to_be_removed] == 0:
                 self.__item_dict.pop(item_to_be_removed)
-
 
     def has_item(self, test_item):
 
@@ -83,14 +84,16 @@ class Character:
         else:
             return False
 
-
     def how_many(self, amount_of_test_items):
 
+        # If character doesn't have the item in the dictionary then zero it
+        # returned.
         if amount_of_test_items not in self.__item_dict:
             return 0
+
+        # Otherwise the item's value is returned.
         else:
             return self.__item_dict.get(amount_of_test_items)
-
 
     def pass_item(self, item, target):
         """
@@ -106,23 +109,30 @@ class Character:
         """
 
         if item in self.__item_dict:
+
+            # If character already has the given item, the item's value is grown
+            # by one.
             if item in target.__item_dict:
                 target.__item_dict[item] += 1
 
+            # If character doesn't have the given item, a new key-value pair it
+            # added to the dictionary __items_dict.
             else:
                 target.__item_dict[item] = 1
 
             if item in self.__item_dict:
+                # If character already has the given item, the item's value
+                # is reduced by one.
                 self.__item_dict[item] -= 1
 
+                # If character had only one of the items that was reduced,
+                # the item itself is removed from the dictionary.
                 if self.__item_dict[item] == 0:
                     self.__item_dict.pop(item)
 
+            return True
 
-
-
-        # TODO: implementation of the method
-
+        return False
 
     def attack(self, target, weapon):
         """
@@ -153,7 +163,27 @@ class Character:
                  False, if attack fails for any reason.
         """
 
-        # TODO: the implementation of the method
+        if weapon not in WEAPONS:
+            print(f"Attack fails: unknown weapon \"{weapon}\".")
+            return
+
+        elif self.__name == target.__name:
+            print(f"Attack fails: {self.__name} can't attack him/herself.")
+            return
+
+        elif weapon not in self.__item_dict:
+            print(f"Attack fails: {self.__name} doesn't have \"{weapon}\".")
+            return
+
+        else:
+            print(f"{self.__name} attacks {target.__name} delivering "
+                  f"{WEAPONS[weapon]} damage.")
+
+            # Weapons damage is reduced from the character's hitpoints.
+            target.__hitpoints -= WEAPONS[weapon]
+
+            if target.__hitpoints < 0:
+                print(f"{self.__name} successfully defeats {target.__name}.")
 
 
 WEAPONS = {
@@ -165,11 +195,10 @@ WEAPONS = {
     "sword":             7,
 }
 
-
 def main():
+
     conan = Character("Conan the Barbarian", 10)
     deadpool = Character("Deadpool", 45)
-
 
     # Testing the pass_item method
 
@@ -190,22 +219,30 @@ def main():
     conan.printout()
     deadpool.printout()
 
-
     # Testing a fight i.e. the attack method
 
     print("-" * 5, "Let's see how a fight proceeds", "-" * 32)
 
     # Conan's turn
-    conan.attack(deadpool, "sword") # Conan doesn't have a sword.
-    conan.attack(conan, "gun")      # A character is not allowed to attack himself.
-    conan.attack(conan, "pen")      # Pen is not a known weapon in WEAPONS dict.
-    conan.attack(deadpool, "gun")   # Attack with a gun.
+    # Conan doesn't have a sword.
+    conan.attack(deadpool, "sword")
+
+    # A character is not allowed to attack himself.
+    conan.attack(conan, "gun")
+
+    # Pen is not a known weapon in WEAPONS dict.
+    conan.attack(conan, "pen")
+
+    # Attack with a gun.
+    conan.attack(deadpool, "gun")
 
     # Deadpool retaliates
-    deadpool.attack(conan, "sword") # Deadpool has a sword.
+    # Deadpool has a sword.
+    deadpool.attack(conan, "sword")
 
     # Conan's 2nd turn
-    conan.attack(deadpool, "gun")   # Attack with a gun again.
+    # Attack with a gun again.
+    conan.attack(deadpool, "gun")
 
     # Deadpool strikes back again and Conan drops "dead".
     deadpool.attack(conan, "sword")
@@ -216,7 +253,6 @@ def main():
 
     conan.printout()
     deadpool.printout()
-
 
 if __name__ == "__main__":
     main()
